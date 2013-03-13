7ff50bd78b4964faf08ff7c7f78a9f92004bbc72
ohnuma <ohnuma@netw.co.jp>
2013-03-13 09:06:14 +0900

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pes::Application.config.secret_token = '89f4173c952c5110b658184392a17c3887438e64bebfa5a2bbf7974bed84d7e966902682e71c87cbae8b077b69b5d24e8612e4f607e696ee334c8c09e5973743'
